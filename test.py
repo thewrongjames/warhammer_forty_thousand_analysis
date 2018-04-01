1a733a580100ebe7b2c8e9f2e65f44f2a19e533d
James Wright <james.wright42@outlook.com>
2018-04-01 12:40:29 +1000

    Add meaningful example

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-import __init__.py as warhammer_forty_thousand_analysis
+from __init__ import Model, Weapon, D6, D3
+
+model = Model({'WS': 4, 'S': 4, 'T': 4, 'A': D3})
+weapon = Weapon({'D': D3 + 2 * D6, 'is_melee': True})
+
+print(float(model.get_average_damage_output(model, weapon)))
